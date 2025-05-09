59f23bb282cd10eb35d3444461b029859f2d9957
Charis <acharisd1200@alumno.ipn.mx>
2025-05-08 19:36:37 -0600

    Backend brute payload

--- backend/main.py
+++ backend/main.py
@@ -1,40 +1,15 @@
-from database import create_db
-from ingester import start_ingest
 from fastapi import FastAPI
-<<<<<<< HEAD
 from fastapi.middleware.cors import CORSMiddleware
-=======
-import json
->>>>>>> wolf-dev
+from database import create_db, Solar, Wind, Battery, Measure, engine
+from sqlmodel import Session, select
+from ingester import start_ingest
+from datetime import datetime
+
 
 #create_db()
 #start_ingest()
 
-data = {
-        "voltmeters": {
-            "solar": 0.0,
-            "wind": 0.0,
-            "battery": 0.0
-            },
-        "ammeters": {
-            "source": 0.0,
-            "battery": 0.0
-            },
-        "thermometers": {
-            "temp1": 0.0,
-            "temp2": 0.0,
-            "temp3": 0.0
-            },
-        "switches": {
-            "solar": False,
-            "wind": False,
-            "battery": True
-            }
-        }
-
 app = FastAPI()
-
-<<<<<<< HEAD
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -42,11 +17,38 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def testo():
-=======
-@app.get("/")
-def testo():
-    payload = json.dumps(data)
->>>>>>> wolf-dev
-    return data
+@app.get("/measures/")
+async def read_measure(start_date: str = "2025-05-08T12:00:00"):
+    date = datetime.fromisoformat(start_date)
+    payload = {
+            "voltmeters": {
+                "solar": [],
+                "wind": [],
+                "battery": []
+                },
+            "ammeters": {
+                "source": [],
+                "battery": []
+                },
+            "thermometers": {
+                "temp1": [],
+                "temp2": [],
+                "temp3": []
+                },
+            }
+
+    with Session(engine) as session:
+        print(date)
+        statement = select(Measure).where(Measure.date >= date)
+        measures = session.exec(statement).all()
+        for measure in measures:
+            payload['voltmeters']['solar'].append(measure.solar.voltage)
+            payload['voltmeters']['wind'].append(measure.wind.voltage)
+            payload['voltmeters']['battery'].append(measure.battery.voltage)
+            payload['ammeters']['source'].append(measure.source_current)
+            payload['ammeters']['battery'].append(measure.battery.current)
+            payload['thermometers']['temp1'].append(measure.battery.temp_a)
+            payload['thermometers']['temp2'].append(measure.battery.temp_b)
+            payload['thermometers']['temp3'].append(measure.battery.temp_c)
+
+    return payload
