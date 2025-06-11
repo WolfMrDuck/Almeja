3e82a0f427aab34367a1ac7e0fb5f0e9920cc82e
Charis <acharisd1200@alumno.ipn.mx>
2025-06-11 14:04:11 -0600

    Live status
    Returns last measure to let user see what is happening.

--- backend/main.py
+++ backend/main.py
@@ -5,8 +5,8 @@ from sqlmodel import Session, select
 from ingester import start_ingest
 from datetime import datetime
 
-create_db()
-start_ingest()
+#create_db()
+#start_ingest()
 
 app = FastAPI()
 
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 @app.get("/measures/")
-async def read_measure(start_date: str = "2025-05-08T12:00:00"):
+async def read_measure_interval(start_date: str = "2025-05-08T12:00:00"):
     date = datetime.fromisoformat(start_date)
     payload = {
             "voltmeters": {
@@ -49,5 +49,52 @@ async def read_measure(start_date: str = "2025-05-08T12:00:00"):
             payload['thermometers']['temp1'].append(measure.battery.temp_a)
             payload['thermometers']['temp2'].append(measure.battery.temp_b)
             payload['thermometers']['temp3'].append(measure.battery.temp_c)
+
+    return payload
+
+@app.get("/live/")
+async def read_last_measure():
+    payload = {
+            "measure": int,
+            "voltage": {
+                "solar": float,
+                "wind": float,
+                "battery": float
+                },
+            "current": {
+                "source": float,
+                "battery": float
+                },
+            "temperature": {
+                "temp1": float,
+                "temp2": float,
+                "temp3": float
+                },
+            "status": {
+                "solar": bool,
+                "wind": bool,
+                "battery": bool,
+                "load": bool,
+                "vca": bool
+                },
+            }
+
+    with Session(engine) as session:
+        statement = select(Measure).order_by(Measure.id.desc()).limit(1)
+        measure = session.exec(statement).one()
+        payload['measure'] = measure.id
+        payload['voltage']['solar'] = measure.solar.voltage
+        payload['voltage']['wind'] = measure.wind.voltage
+        payload['voltage']['battery'] = measure.battery.voltage
+        payload['current']['source'] = measure.source_current
+        payload['current']['battery'] = measure.battery.current
+        payload['temperature']['temp1'] = measure.battery.temp_a
+        payload['temperature']['temp2'] = measure.battery.temp_b
+        payload['temperature']['temp3'] = measure.battery.temp_c
+        payload['status']['solar'] = measure.solar.status
+        payload['status']['wind'] = measure.wind.status
+        payload['status']['battery'] = measure.battery.status
+        payload['status']['load'] = measure.load_status
+        payload['status']['vca'] = measure.vca_status
 
     return payload
